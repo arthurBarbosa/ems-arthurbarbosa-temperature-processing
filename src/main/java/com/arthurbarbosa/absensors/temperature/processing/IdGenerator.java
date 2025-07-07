9969162204a1252bd6c0ea22a606b7943eed93eb
Arthur Barbosa <arthurbruno03@gmail.com>
2025-07-07 15:15:47 -0300

    Created method POST Sensor

---
+++ src/main/java/com/arthurbarbosa/absensors/temperature/processing/IdGenerator.java
@@ -0,0 +1,18 @@
+package com.arthurbarbosa.absensors.temperature.processing;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedEpochRandomGenerator;
+
+import java.util.UUID;
+
+public class IdGenerator {
+    private static final TimeBasedEpochRandomGenerator timeBasedEpochRandomGenerator = Generators.timeBasedEpochRandomGenerator();
+
+    private IdGenerator() {
+
+    }
+
+    public static UUID generateTimeBaseUUID() {
+        return timeBasedEpochRandomGenerator.generate();
+    }
+}
